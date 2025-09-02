d82ae4ab98cbf3b6c8f8ec26b68e894a2b233cea
Thomas Eberl <thomas.eberl@zeiss.com>
2025-09-02 15:19:54 +0200

    setup with aspire part 2

--- AspireWithAngular/AspireWithAngular.AppHost/AppHost.cs
+++ AspireWithAngular/AspireWithAngular.AppHost/AppHost.cs
@@ -1,6 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireWithAngular_ApiService>("apiservice")
+    .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health");
+
+builder.AddNpmApp("angular", "../AspireWithAngular.Web")
+    .WithReference(apiService)
+    .WaitFor(apiService)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
